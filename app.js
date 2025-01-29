6d8f8ef8802a734fd77a8357764c84528f2adce4
Adryan Gabriel <Adryangabriel6@gmail.com>
2025-01-29 20:11:00 -0300

    Parte 2, começando a sortear

--- app.js
+++ app.js
@@ -11,6 +11,36 @@ if (nome === "") {
     alert('Por favor, insira um nome.');
     return;
 }
+
+//Ver se o nome não está repetido
+if(amigos.includes(nomeAmigo)) {
+    alert(`O nome ${nomeAmigo} já está adicionado na lista`);
+    return;
+}
   
+//Adicionar o nome ao array de amigos
+amigos.push(nomeAmigo);
+
+
+//Limpar o campo de entrada
+inputAmigo.ariaValu = "";
+
+//Atualizar a lista
+atualizarLista();
 }
 
+function atualizarLista() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ''; // Limpar o campo
+    for(let i = 0; i <amigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li) // Criar sequência na lista
+
+    }
+
+}
+
+function sortearAmigo() {
+    if(amigos)
+}
